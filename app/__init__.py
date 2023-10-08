917c3ff1275c7e045a9eb5c713146b9950967dfe
Administrator <kristofer.souza2027@hotmail.com>
2023-10-07 20:22:32 -0800

    Erro de diretório resolvido, agora temos caminhos absolutos

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
+import os
 from pygame import mixer
+
+# Definição de caminhos absolutos
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+
+caminho_click_button = os.path.join(diretorio_atual, 'media', 'sounds', 'click_button.mp3')
+caminho_tuin = os.path.join(diretorio_atual, 'media', 'sounds', 'Tuin_Editado.MP3')
+caminho_startGame = os.path.join(diretorio_atual, 'media', 'sounds', 'Start_Editado01.MP3')
+caminho_back_button = os.path.join(diretorio_atual, 'media', 'sounds', 'back_button.MP3')
+caminho_finish_time = os.path.join(diretorio_atual, 'media', 'sounds', 'finish_time.MP3')
 
 # Sounds
 mixer.init()
-click_button = mixer.Sound(r'app\media\sounds\click_button.mp3')
-tuin = mixer.Sound(r'app\media\sounds\Tuin_Editado.MP3')
-startGame = mixer.Sound(r'app\media\sounds\Start_Editado01.MP3')
-back_button = mixer.Sound(r'app\media\sounds\back_button.MP3')
-finish_time = mixer.Sound(r'app\media\sounds\finish_time.MP3')
+click_button = mixer.Sound(caminho_click_button)
+tuin = mixer.Sound(caminho_tuin)
+startGame = mixer.Sound(caminho_startGame)
+back_button = mixer.Sound(caminho_back_button)
+finish_time = mixer.Sound(caminho_finish_time)
